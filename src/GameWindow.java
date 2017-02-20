41811a09bb3cedb83030f9614464c707f1ac45fc
leuducanh <leuaction@gmail.com>
2017-02-20 22:39:56 +0700

    add move

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,12 +1,19 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 
 /**
  * Created by l on 2/19/2017.
  */
 public class GameWindow extends Frame{
+
+    Image backgroundImage;
+    Image planeImage;
+    Image landImage;
+    private int planeX,planeY;
 
     public GameWindow(){
         setVisible(true);
@@ -26,5 +33,83 @@ public class GameWindow extends Frame{
                 System.out.println("Window closed");
             }
         });
+
+        //1:load image
+
+        //2:draw image
+            backgroundImage = loadImageFromRes("background.png");
+            planeImage = loadImageFromRes("plane4.png");
+            landImage = loadImageFromRes("island.png");
+
+        planeX = (400 - 45)/2;
+        planeY = 600 - 35;
+
+            update(getGraphics());
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                int x, y;
+                x = planeX;
+                y = planeY;
+                switch (e.getKeyCode()){
+                    case (KeyEvent.VK_RIGHT):{
+                        planeX += 5;
+                        break;
+                    }
+                    case (KeyEvent.VK_LEFT):{
+                        planeX -= 5;
+                        break;
+                    }
+                    case (KeyEvent.VK_UP):{
+                        planeY -= 5;
+                        break;
+                    }
+                    case (KeyEvent.VK_DOWN):{
+                        planeY += 5;
+                        break;
+                    }
+                }
+                if(!checkCoordinatePlane()){
+                    planeX = x;
+                    planeY = y;
+                }
+                repaint();
+                repaint();
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+
+            }
+        });
+
+    }
+
+
+
+    @Override
+    public void update(Graphics g) {
+        g.drawImage(backgroundImage,0,0,400,600,null);
+        g.drawImage(planeImage,planeX,planeY,45,35,null);
+    }
+
+    private boolean checkCoordinatePlane(){
+        if((planeX > 400 - 45) || (planeX < 0) || (planeY > 600 - 35) || (planeY < 35)) {
+            return false;
+        }
+        return true;
+    }
+
+    private Image loadImageFromRes(String url){
+        try {
+            Image image = ImageIO.read(new File("resources/" + url));
+            return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
